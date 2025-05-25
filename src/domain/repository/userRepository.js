3a56a67627e2c8ebcc6d065a228995ec128f95a2
AryKurnia <aryk922@gmail.com>
2025-05-25 19:46:40 +0800

    chore: rename file to follow consistent naming convention

--- src/domain/repository/userRepository.js
+++
@@ -1,7 +0,0 @@
-class UserRepository {
-  static async addUser(_user) { throw new Error('Not implemented'); }
-
-  static async getUserByEmail(_email) { throw new Error('Not implemented'); }
-}
-
-module.exports = UserRepository;
